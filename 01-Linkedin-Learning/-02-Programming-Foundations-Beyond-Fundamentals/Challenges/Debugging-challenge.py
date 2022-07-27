5556040658545b6615d23bdfd09fc3e74d0d0763
Mohammed ALDAHER <81594456+daher29@users.noreply.github.com>
2022-07-27 21:03:12 +0300

    Update Debugging-challenge.py

--- 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challenges/Debugging-challenge.py
+++ 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challenges/Debugging-challenge.py
@@ -1,1 +1,20 @@
+def plant_recommendation(care):
+  if care = 'low':
+    print('aloe')
+  elif care == 'medium':
+    print('pothos')
+  elif care == 'medium':
+    print('orchid')
 
+
+
+plant_rec('low')
+plant_recommendation('medium')
+plant_recommendation('high')
+
+
+# line 2 there is syntax error "=" doesn't mean equal, should use "=="
+
+# Line 10 there is a runtime error, should type the function correctly and completely "plant_recommendation"
+
+# Line 6 there is a logic error, should be 'high' instead of 'medium'
