69f443de6090a5481944ec943481fd86a69953d8
Mohammed ALDAHER <81594456+daher29@users.noreply.github.com>
2022-07-27 20:59:03 +0300

    Update Guessing-Game-Challenge.py

--- 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/Guessing-Game-Challenge.py
+++ 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/Guessing-Game-Challenge.py
@@ -1,1 +1,14 @@
 
+print("Hello there!")
+
+name = input("What's your name?")
+print("Hello, ", name, "nice to meet you.")
+
+mff = input("What's my favorite food?")
+
+if (mff == "spaghetti"):
+  print("Yep! So amazing!")
+else:
+  print("Yuck! That's not it!")
+  
+print("Thanks for playing!")
